616c98d66dbe82f5e255f9eb8d9c9b314fd37858
ShemikaKW <Shemikakw@gmail.com>
2019-01-13 18:08:14 -0500

    Everything working

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import React, { Component} from 'react';
+import './App.css';
+import Card from "./components/Card"
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
+import characters from "./characters.json";
+import Container from "./Container"
+import Column from "./Column";
+import Row from "./Row";;
+
+
+function shuffleCharacters(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+class App extends Component {
+  // Set this.state
+  state = {
+    characters,
+    currentScore: 0,
+    topScore: 0,
+    rightWrong: "",
+    clicked: [],
+  };
+
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
+
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      rightWrong: ""
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    }
+    else if (newScore === 12) {
+      this.setState({ rightWrong: "You win!" });
+    }
+    this.handleShuffle();
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      rightWrong: "You guessed incorrectly!",
+      clicked: []
+    });
+    this.handleShuffle();
+  };
+
+  handleShuffle = () => {
+    let shuffledCharacters = shuffleCharacters(characters);
+    this.setState({ characters: shuffledCharacters });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Navbar
+          title="Batman Clicky Game"
+          score={this.state.currentScore}
+          topScore={this.state.topScore}
+          rightWrong={this.state.rightWrong}
+        />
+
+        <Header>
+          Try to click on each character without clicking any duplicates.
+          
+        </Header>
+
+        <Container>
+          <Row>
+            {this.state.characters.map(character => (
+              <Column size="md-3 sm-6">
+                <Card
+                  key={character.id}
+                  handleClick={this.handleClick}
+                  handleIncrement={this.handleIncrement}
+                  handleReset={this.handleReset}
+                  handleShuffle={this.handleShuffle}
+                  id={character.id}
+                  image={character.image}
+                />
+              </Column>
+            ))}
+          </Row>
+        </Container>
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
